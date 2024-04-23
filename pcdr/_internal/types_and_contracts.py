608baaaaa1d436f1de9824738ce568fe0a277925
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2024-04-23 15:57:03 -0400

    reorg imports so that `import pcdr` without gnuradio installed only gives warning (rather than raising exception)

--- pcdr/_internal/types_and_contracts.py
+++ pcdr/_internal/types_and_contracts.py
@@ -1,7 +1,5 @@
 from typing import Union, TypeVar, Protocol, Callable
 import typing
-from gnuradio import gr
-from PyQt5 import Qt
 
 
 
@@ -10,17 +8,26 @@ TRealNum = TypeVar('TRealNum', int, float)
 
 TRealOrComplexNum = TypeVar('TRealOrComplexNum', int, float, complex)
 
-class SupportsQueueSink(Protocol):
-    queue_sink: "queue_sink"
 
-
-class top_block_and_widget(gr.top_block, Qt.QWidget):
-    pass
 
 # TODO: incorporate this # @typing.runtime_checkable
 class HasWorkFunc(Protocol):
     work: Callable
 
 
-## avoid circular imports
-from pcdr._internal.our_GR_blocks import queue_sink
+## avoid circular imports, and avoid gnuradio dependency 
+## when its unneeded
+try:
+    from gnuradio import gr
+    from PyQt5 import Qt
+
+    class top_block_and_widget(gr.top_block, Qt.QWidget):
+        pass
+    
+    class SupportsQueueSink(Protocol):
+        queue_sink: "queue_sink"
+
+    from pcdr._internal.our_GR_blocks import queue_sink
+
+except ModuleNotFoundError:
+    pass    
