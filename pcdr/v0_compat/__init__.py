76098e959ae10621f184386f4d052729457d331b
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2024-05-09 16:01:20 -0400

    add v0_compat (unfinished)

---
+++ pcdr/v0_compat/__init__.py
@@ -0,0 +1,5 @@
+def makeWave(copy, paste, the, exact, args):
+    """
+    The old version of make_wave. Will be removed from one of the v1.x releases sometime in the first half of the year 2025.
+    """
+    implementation_here
